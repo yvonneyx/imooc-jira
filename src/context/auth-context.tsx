89e0a98dab78a2ad213ad22c21417a7fef97ea5c
yvonneyx <zhuyuxin0627@gmail.com>
2023-03-08 20:05:28 +0800

    feat: 添加登录状态维持

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,11 +1,23 @@
-import React, { useState, ReactNode } from "react";
-import * as auth from "auth-provider";
-import { User } from "screens/project-list/search-panel";
+import React, { useState, ReactNode } from 'react';
+import * as auth from 'auth-provider';
+import { User } from 'screens/project-list/search-panel';
+import { http } from 'utils/http';
+import { useMount } from 'utils';
 
 interface AuthForm {
   username: string;
   password: string;
 }
+
+const bootstrapUser = async () => {
+  let user = null;
+  const token = auth.getToken();
+  if (token) {
+    const data = await http('me', { token });
+    user = data.user;
+  }
+  return user;
+};
 
 const AuthContext = React.createContext<
   | {
@@ -16,14 +28,18 @@ const AuthContext = React.createContext<
     }
   | undefined
 >(undefined);
-AuthContext.displayName = "AuthContext";
+AuthContext.displayName = 'AuthContext';
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
 
   const login = (form: AuthForm) => auth.login(form).then(setUser);
   const register = (form: AuthForm) => auth.register(form).then(setUser);
-  const logout = () => auth.logout().then((user) => setUser(null));
+  const logout = () => auth.logout().then(() => setUser(null));
+
+  useMount(() => {
+    bootstrapUser().then(setUser);
+  });
 
   return (
     <AuthContext.Provider
@@ -36,7 +52,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 export const useAuth = () => {
   const context = React.useContext(AuthContext);
   if (!context) {
-    throw new Error("useAuth必须在AuthProvider中使用");
+    throw new Error('useAuth必须在AuthProvider中使用');
   }
   return context;
 };
--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,36 +1,26 @@
-import { SearchPanel } from "./search-panel";
-import { List } from "./list";
-import React, { useState, useEffect } from "react";
-import { cleanObject, useMount, useDebounce } from "utils";
-import * as qs from "qs";
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import { SearchPanel } from './search-panel';
+import { List } from './list';
+import React, { useState, useEffect } from 'react';
+import { cleanObject, useMount, useDebounce } from 'utils';
+import { useHttp } from 'utils/http';
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
-    name: "",
-    personId: "",
+    name: '',
+    personId: '',
   });
+  const debounceParam = useDebounce(param, 200);
   const [users, setUsers] = useState([]);
   const [list, setList] = useState([]);
+  const client = useHttp();
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client('users').then(setUsers);
   });
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
-      }
-    );
-  }, [useDebounce(param, 200)]);
+    client('projects', { data: cleanObject(param) }).then(setList);
+  }, [debounceParam]);
 
   return (
     <div>
